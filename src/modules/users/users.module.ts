a80ae53543bbe3d07851306fa5b1949360db182b
XSIJIE975 <xsijie975@qq.com>
2025-10-27 21:20:36 +0800

    refactor(users): 移除全局模块重复导入
    - 移除 DatabaseModule 的重复导入
    - 由于 DatabaseModule 已标记为 @Global()，无需在业务模块中重复导入

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,11 +1,10 @@
 import { Module, forwardRef } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
-import { DatabaseModule } from '../../shared/database/database.module';
 import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [DatabaseModule, forwardRef(() => AuthModule)],
+  imports: [forwardRef(() => AuthModule)],
   controllers: [UsersController],
   providers: [UsersService],
   exports: [UsersService],
