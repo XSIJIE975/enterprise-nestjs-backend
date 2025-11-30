583ec320641ef00746350f608715c0478ed1a58b
XSIJIE975 <xsijie975@qq.com>
2025-11-30 13:35:07 +0800

    feat(dto): 更新 PaginatedDto 文档，添加示例并调整 data 属性注释

--- src/common/dtos/paginated.dto.ts
+++ src/common/dtos/paginated.dto.ts
@@ -4,12 +4,20 @@ import { PaginationMetaDto } from './pagination-meta.dto';
 /**
  * 通用分页响应 DTO 基类
  * 使用泛型 T 来支持任何数据类型
+ *
+ * 注意：子类需要使用 @ApiProperty 重新声明 data 属性来指定具体的类型
+ *
+ * @example
+ * export class UserPageVo extends PaginatedDto<UserVo> {
+ *   @ApiProperty({ type: [UserVo], description: '用户列表' })
+ *   declare data: UserVo[];
+ * }
  */
 export class PaginatedDto<T> {
-  @ApiProperty({
-    description: '数据列表',
-    type: Array,
-  })
+  /**
+   * 数据列表
+   * 子类应该使用 @ApiProperty 装饰器重新声明此属性以指定具体类型
+   */
   data: T[];
 
   @ApiProperty({
